86ea9b68f424cce9cc033d6ab6abf209f3025df3
Apik <apocalypsesss@yandex.ru>
2016-10-05 20:38:19 +0600

    Тестирование программы с 5 параметрами, 25м, 20ж фото

--- lab1/gender_recognition.py
+++ lab1/gender_recognition.py
@@ -6,8 +6,6 @@ from pybrain.structure import TanhLayer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
     
-
-
 def pixel_camparision(img):
     list = []
     simple_features = []
@@ -26,23 +24,29 @@ def pixel_camparision(img):
                     simple_features.append(int(math.fabs(val_i-val_j)<10))
                     simple_features.append(int(math.fabs(val_i-val_j)<25))
                     simple_features.append(int(math.fabs(val_i-val_j)<50))
-    print(len(simple_features))  
+                    """simple_features.append(int(val_i-val_j<0))
+                    simple_features.append(int(math.fabs(val_i-val_j)>5))
+                    simple_features.append(int(math.fabs(val_i-val_j)>10))
+                    simple_features.append(int(math.fabs(val_i-val_j)>25))
+                    simple_features.append(int(math.fabs(val_i-val_j)>50))"""
+    #print(len(simple_features))  
     return simple_features
+    #return list
 
 def do_train_dataset(dataset):
     
     os.chdir('./train_set/male')
-    
+    print('Working ',end=' ')
     for i in os.listdir("./"):
-        print(i)
+        print('.',end=' ')
         img = cv2.imread(i, cv2.IMREAD_GRAYSCALE)
         list = pixel_camparision(img)    
         dataset.addSample(list, (1, ))
     
-    
-    os.chdir('./train_set/famale')
+    os.chdir('../famale')
     
     for i in os.listdir("./"):
+        print('.',end=' ')
         img = cv2.imread(i, cv2.IMREAD_GRAYSCALE)
         list = pixel_camparision(img)
         dataset.addSample(list, (0, ))
@@ -50,21 +54,29 @@ def do_train_dataset(dataset):
     return dataset
 
 def main():
-    
-   
-    
+
+    print ('20 f, 25 m, 5 parameters')
     net = buildNetwork(798000, 3, 1)
     dataset = SupervisedDataSet(798000, 1)
     dataset = do_train_dataset(dataset)
  
     trainer = BackpropTrainer(net, dataset)
-    trainer.trainUntilConvergence()
-    for epoch in range(1):
+    #trainer.trainUntilConvergence()
+    for epoch in range(50):
         trainer.train()
+        print('.',end=' ')
+    print()
+    print ('Value of error = ',trainer.train())
+    #print (trainer.trainUntilConvergence())
+    
         
-        
-        
-        
+    os.chdir('../test')
+    
+    for i in os.listdir("./"):
+        print(i)
+        img = cv2.imread(i, cv2.IMREAD_GRAYSCALE)
+        list = pixel_camparision(img)
+        print(net.activate(list))
     
 if __name__=="__main__":
     main()
